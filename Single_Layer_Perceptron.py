1f140e7ed639fed6e75547a4490a838084bf11e7
Woody_K <woody35545@gmail.com>
2022-03-19 01:02:55 +0900

    [+] Single_Layer_Perceptron
    > working..

---
+++ Single_Layer_Perceptron.py
@@ -0,0 +1,25 @@
+import numpy as np
+import csv # for data load
+import time
+
+# Hyper Parameters
+RND_MEAN = 0 # 정규분포 난수값의 평균
+RND_STD = 0.0030 # 정규분포 난수값의 표준편차
+
+# main function
+def exec(epoch_count = 10, mb_size = 10, report =1):
+    # epoch_count = 학습횟수
+    # mb_size = 미니 배치 크기
+    # report = ;
+    load_dataset()
+    init_model()
+    train_and_test(epoch_count, mb_size, report)
+
+def load_dataset():
+    None
+
+def init_model():
+    None
+
+def train_and_test():
+    None
